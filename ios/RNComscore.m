05ec543f4be052d8dbd69030504f75682c6744f2
Jan Lievens <jan.lievens@gmail.com>
2017-06-15 12:18:37 +0200

    trackview and trackevent methods

--- ios/RNComscore.m
+++ ios/RNComscore.m
@@ -7,17 +7,82 @@
 #import <React/RCTLog.h>
 #endif
 
-@implementation RNComscore
+@implementation RNComscore {
+
+}
+
+
+NSString *comScoreAppName;
+NSString *comScoreCustomerC2;
+NSString *comScorePublisherSecret;
+NSString *comScorePixelUrl;
+
+- (instancetype)init
+{
+    if ((self = [super init])) {
+        
+        
+        // Start ComScore
+        // SELogDebug(@"[ComScore]:\nCSAppName:%@\nCSCustomerC2:%@\nCSPublisherSecret:%@\nCSPixelUrl:%@\n",
+                   //CSAppName, CSCustomerC2, CSPublisherSecret, CSPixelUrl);
+        
+        // SELogDebug(@"[ComScore] startTracker for CustomerC2: %@", CSCustomerC2);
+        
+      
+        
+        comScoreAppName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"ComScoreAppName"];
+        comScorePublisherSecret = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"ComScorePublisherSecret"];
+        comScorePixelUrl = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"ComScorePixelUrl"];
+        
+        [CSComScore setAppContext];
+        [CSComScore setAppName:comScoreAppName];
+        [CSComScore setPublisherSecret:comScorePublisherSecret];
+        [CSComScore setPixelURL:comScorePixelUrl];
+        
+        NSMutableDictionary *labels = [NSMutableDictionary dictionary];
+        labels[@"category"] = @"karrewiet";
+        labels[@"behoefte"] = @"verbindend";
+        labels[@"doelgroep"] = @"kind";
+        labels[@"marketing"] = @"none";
+        labels[@"mediatype"] = @"tv";
+        labels[@"productiehuis"] = @"Small Town Heroes";
+        labels[@"waar"] = @"app";
+        
+        //  SELogDebug(@"%@", labels);
+        [CSComScore setLabels:labels];
+        
+    }
+    return self;
+}
 
 - (dispatch_queue_t)methodQueue
 {
-  return dispatch_queue_create("com.facebook.React.AsyncLocalStorageQueue", DISPATCH_QUEUE_SERIAL);
+    return dispatch_queue_create("com.facebook.React.AsyncLocalStorageQueue", DISPATCH_QUEUE_SERIAL);
 }
+
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(trackView:(NSString *) view)
+{
+    
+    // slashes become dots
+    NSString *dottedView = [view stringByReplacingOccurrencesOfString:@"/" withString:@"."];
+    // all lowercase
+    //NSString *comscoreViewName = [[CSAppName stringByAppendingString:dottedView ] lowercaseString];
+    NSString *comscoreViewName = [CSAppName stringByAppendingString:dottedView ];
+    
+    [CSComScore viewWithLabels:@{@"name":comscoreViewName }];
+}
+
+RCT_EXPORT_METHOD(trackEvent:(NSString *)action category:(NSString *)category)
+{
+    NSString *eventName = [NSString stringWithFormat:@"%@.%@",category,action];
+    [CSComScore hiddenWithLabels:@{@"name": eventName}];
+}
 
 RCT_EXPORT_METHOD(show:(NSString *)type location:(NSString *)message)
 {
-  RCTLogInfo(@"Pretending to show %@ at %@", type, message);
+    RCTLogInfo(@"Pretending to show %@ at %@", type, message);
 }
 
 @end
