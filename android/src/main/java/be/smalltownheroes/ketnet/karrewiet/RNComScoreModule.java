99b566c887b68177a41056c9181738f2a92964e8
Jan Lievens <jan.lievens@gmail.com>
2017-06-15 16:54:20 +0200

    comscore lib calls for android

--- android/src/main/java/be/smalltownheroes/ketnet/karrewiet/RNComScoreModule.java
+++ android/src/main/java/be/smalltownheroes/ketnet/karrewiet/RNComScoreModule.java
@@ -1,13 +1,12 @@
 
 package be.smalltownheroes.ketnet.karrewiet;
 
-import android.widget.Toast;
-
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReadableMap;
+import com.comscore.analytics.comScore;
 
 public class RNComScoreModule extends ReactContextBaseJavaModule {
 
@@ -25,23 +24,50 @@ public class RNComScoreModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void init(ReadableMap options) {
-    String comScoreAppName = options.getString("appName");
+    this.comScoreAppName = options.getString("appName");
     String comScorePublisherSecret = options.getString("publisherSecret");
     String comScorePixelUrl = options.getString("pixelUrl");
-    String text = "INIT: appName: {" + comScoreAppName + "} - publisherSecret: {" + comScorePublisherSecret + "} - pixelUrl: {" + comScorePixelUrl + "}";
-    Toast.makeText(getReactApplicationContext(), text, Toast.LENGTH_LONG).show();
+    try {
+        comScore.setAppContext(getReactApplicationContext());
+        comScore.setAppName(comScoreAppName);
+        comScore.setPublisherSecret(comScorePublisherSecret);
+        comScore.setPixelURL(comScorePixelUrl);
+        HashMap<String, String> labels = new HashMap<String, String>();
+        labels.put("category", "app");
+        labels.put("behoefte", "verbindend");
+        labels.put("doelgroep", "none");
+        labels.put("marketing", "none");
+        labels.put("mediatype", "tv");
+        labels.put("productiehuis", "Small Town Heroes");
+        labels.put("waar", "app");
+        comScore.setLabels(labels);
+      } catch (Exception ex) {
+      	// fail silently for now
+      }
+    }
+    // String text = "INIT: appName: {" + comScoreAppName + "} - publisherSecret: {" + comScorePublisherSecret + "} - pixelUrl: {" + comScorePixelUrl + "}";
+    // Toast.makeText(getReactApplicationContext(), text, Toast.LENGTH_LONG).show();
   }
 
   @ReactMethod
   public void trackView(String view) {
-    String text = "TRACKVIEW: view: {" + view + "}";
-    Toast.makeText(getReactApplicationContext(), text, Toast.LENGTH_LONG).show();
+  	try {
+	    String comScoreViewName = this.comScoreAppName + viewName;
+	    HashMap<String, String> labels = new HashMap<String, String>();
+	    labels.put("name", comScoreViewName.replace("/", "."));
+	    comScore.view(labels);
+	  } catch (Exception ex) {
+    	// fail silently for now
+    }
+    // String text = "TRACKVIEW: view: {" + view + "}";
+    // Toast.makeText(getReactApplicationContext(), text, Toast.LENGTH_LONG).show();
   }
 
   @ReactMethod
   public void trackEvent(String action, String category) {
-    String text = "TRACKEVENT: action: {" + action + "} - category: {" + category + "}";
-    Toast.makeText(getReactApplicationContext(), text, Toast.LENGTH_LONG).show();
+  	// not implemented yet
+    // String text = "TRACKEVENT: action: {" + action + "} - category: {" + category + "}";
+    // Toast.makeText(getReactApplicationContext(), text, Toast.LENGTH_LONG).show();
   }
 
 }
