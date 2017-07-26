5df485aa13a22da271260aa2ad13238ca59db2ae
Jan Lievens <jan.lievens@gmail.com>
2017-07-26 12:50:37 +0200

    still from barcelona

--- ios/RNComscore.m
+++ ios/RNComscore.m
@@ -12,6 +12,16 @@ static CSStreamSense *streamSense;
 
 @implementation RNComScore {
 
+	+ (NSString*)convertDateForStreamSense:(NSString*)dateString {
+		if (dateString == nil) return @"";
+		NSDateFormatter *formatter = [NSDateFormatter new];
+		[formatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSz"];
+		NSDate *date = [formatter dateFromString:dateString];
+		[formatter setDateFormat:@"YYYYMMdd"];
+		NSString *formattedString = [formatter stringFromDate:date];
+		return formattedString?formattedString:@"";
+	}
+
 }
 
 NSString *comScoreAppName;
@@ -21,16 +31,6 @@ NSString *comScorePixelUrl;
 NSString *streamSenseMediaPlayer;
 NSString *streamSenseVersion;
 NSString *streamSenseChannel;
-
-+ (NSString*)convertDateForStreamSense:(NSString*)dateString {
-	if (dateString == nil) return @"";
-	NSDateFormatter *formatter = [NSDateFormatter new];
-	[formatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSz"];
-	NSDate *date = [formatter dateFromString:dateString];
-	[formatter setDateFormat:@"YYYYMMdd"];
-	NSString *formattedString = [formatter stringFromDate:date];
-	return formattedString?formattedString:@"";
-}
 
 - (dispatch_queue_t)methodQueue
 {
@@ -93,8 +93,8 @@ RCT_EXPORT_METHOD(trackEvent:(NSString *)action category:(NSString *)category)
 
 RCT_EXPORT_METHOD(trackVideoStreaming:(NSDictionary*)videoInfo category:(NSString *)videoAction)
 {
-	long position = videoInfo[@"position"] ? [videoInfo[@"position"] longValue] : 0;
-	long length =  videoInfo[@"length"] ? [videoInfo[@"length"] longValue] : 0;
+	long position = videoInfo[@"position"] ? [videoInfo[@"position"] longValue] : 0L;
+	long length =  videoInfo[@"length"] ? [videoInfo[@"length"] longValue] : 0L;
 	[streamSense setClip:@ {
 	   @"ns_st_cl": @(length),
 	   @"ns_st_el": @(length),
