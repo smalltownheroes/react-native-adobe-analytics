85a77390c469d3d5fe4322295428fb38f062e1b5
Jan Lievens <jan.lievens@gmail.com>
2017-09-07 17:17:34 +0200

    too fast boy

--- android/src/main/java/be/smalltownheroes/ketnet/karrewiet/RNComScoreModule.java
+++ android/src/main/java/be/smalltownheroes/ketnet/karrewiet/RNComScoreModule.java
@@ -132,8 +132,8 @@ public class RNComScoreModule extends ReactContextBaseJavaModule {
 			playbackLabels.put("ns_st_tp", videoInfo.getString("parts"));
 		}
 		if (!videoInfo.isNull("whatson")) {
-			Log.i("+whatson isNull++++", videoInfo.isNull("whatson");
-			Log.i("-whatson getString----", videoInfo.getString("whatson");
+			Log.i("+whatson isNull++++", videoInfo.isNull("whatson"));
+			Log.i("-whatson getString----", videoInfo.getString("whatson"));
 			playbackLabels.put("ns_st_ci", videoInfo.getString("whatson"));
 			playbackLabels.put("vrt_vid_id", videoInfo.getString("whatson"));
 		}
